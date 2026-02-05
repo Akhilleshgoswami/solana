4b3134291afa9532d77f2de578a514344912a262
Akhilleshgoswami <agiri5375@gmail.com>
2026-02-05 17:47:21 +0530

    done with lending protocal code

--- programs/lending/src/lib.rs
+++ programs/lending/src/lib.rs
@@ -1,18 +1,19 @@
 use anchor_lang::prelude::*;
 use instructions::*;
-mod error;
-mod instructions;
+
 mod state;
-declare_id!("FriDxsrNEzqEqiW45tdTdBQmerPSuGarj1mu9N4RTTZP");
+mod instructions;
+mod error;
+mod constants;
+
+declare_id!("CdZeD33fXsAHfZYS8jdxg4qHgXYJwBQ1Bv6GJyETtLST");
 
 #[program]
-pub mod lending {
+pub mod lending_protocol {
+
     use super::*;
-    pub fn init_bank(
-        ctx: Context<InitBank>,
-        liquidation_threshold: u64,
-        max_ltv: u64,
-    ) -> Result<()> {
+
+    pub fn init_bank(ctx: Context<InitBank>, liquidation_threshold: u64, max_ltv: u64) -> Result<()> {
         process_init_bank(ctx, liquidation_threshold, max_ltv)
     }
 
@@ -20,7 +21,24 @@ pub mod lending {
         process_init_user(ctx, usdc_address)
     }
 
-    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+    pub fn deposit (ctx: Context<Deposit>, amount: u64) -> Result<()> {
         process_deposit(ctx, amount)
     }
+
+    pub fn withdraw (ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        process_withdraw(ctx, amount)
+    }
+
+    pub fn borrow(ctx: Context<Borrow>, amount: u64) -> Result<()> {
+        process_borrow(ctx, amount)
+    }
+
+    pub fn repay(ctx: Context<Repay>, amount: u64) -> Result<()> {
+        process_repay(ctx, amount)
+    }
+
+    pub fn liquidate(ctx: Context<Liquidate>) -> Result<()> {
+        process_liquidate(ctx)
+    }
 }
+
